4634f4d2fcf594336b869d4a6d39e02178cea005
Chase McElroy <ichasem@gmail.com>
2017-03-11 08:57:29 -0800

    added custom classes to Sign In UI

---
+++ SocialApp/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  SocialApp
+//
+//  Created by Chase McElroy on 3/10/17.
+//  Copyright © 2017 Chase McElroy. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
