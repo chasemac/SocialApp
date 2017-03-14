a999aa96035b530d85e3e41cf68cdde13773a538
Chase McElroy <ichasem@gmail.com>
2017-03-13 21:00:33 -0700

    added Facebook auth integrated with Firebase

--- SocialApp/SignInVC.swift
+++ SocialApp/SignInVC.swift
@@ -7,6 +7,10 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
+
 
 class SignInVC: UIViewController {
 
@@ -20,6 +24,32 @@ class SignInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func facebookBtnTapped(_ sender: Any) {
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("CHASE: unable to authenticate with facebook - \(error)")
+                
+            } else if result?.isCancelled == true {
+                print("CHASE: user canceled")
+            } else {
+                print("CHASE: Successfully authenticated with facebook")
+                let credecntial = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credecntial)
+            }
+        }
+    }
+    
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("CHASE: Unable to auth with Firebase - \(error)")
+                
+            } else {
+                print("CHASE: Succesffully authenticated with Firebase")
+            }
+        })
+    }
 
 }
 
