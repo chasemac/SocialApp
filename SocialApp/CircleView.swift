8d5209fbfb95966fe3c7475ffa56bd29d0b62797
Chase McElroy <ichasem@gmail.com>
2017-03-23 14:48:01 -0700

    Added Feed VC UI elements on storyboard

--- SocialApp/CircleView.swift
+++ SocialApp/CircleView.swift
@@ -1,22 +1,26 @@
 //
-//  FancyView.swift
+//  CircleView.swift
 //  SocialApp
 //
-//  Created by Chase McElroy on 3/10/17.
+//  Created by Chase McElroy on 3/23/17.
 //  Copyright © 2017 Chase McElroy. All rights reserved.
 //
 
 import UIKit
 
-class FancyView: UIView {
-
+class CircleView: UIImageView {
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
     }
+
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        layer.cornerRadius = self.frame.width / 2
+    }
+
 }
