1fdc783c6327565f51bb08b96f6a0c766a9eefbc
Chase McElroy <ichasem@gmail.com>
2017-03-24 13:18:20 -0700

    added users auth credentials to Firebase DB

--- SocialApp/SignInVC.swift
+++ SocialApp/SignInVC.swift
@@ -21,8 +21,6 @@ class SignInVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -60,9 +58,9 @@ class SignInVC: UIViewController {
             } else {
                 print("CHASE: Succesffully authenticated with Firebase")
                 if let user = user {
-                    self.completeSignIn(id: user.uid)
+                    let userData = ["provider": credential.provider]
+                    self.completeSignIn(id: user.uid, userData: userData)
                 }
-                
             }
         })
     }
@@ -72,7 +70,8 @@ class SignInVC: UIViewController {
                 if error == nil {
                     print("CHASE: EMAIL User authenticated with Firebase")
                     if let user = user {
-                        self.completeSignIn(id: user.uid)
+                        let userData = ["provider": user.providerID]
+                        self.completeSignIn(id: user.uid, userData: userData)
                     }
                 } else {
                     FIRAuth.auth()?.createUser(withEmail: email, password: pwd, completion: { (user, error) in
@@ -81,9 +80,9 @@ class SignInVC: UIViewController {
                         } else {
                             print("CHASE: Succesffully authentitcated with Firebase email")
                             if let user = user {
-                                self.completeSignIn(id: user.uid)
+                                let userData = ["provider": user.providerID]
+                                self.completeSignIn(id: user.uid, userData: userData)
                             }
-                            
                         }
                     })
                 }
@@ -91,11 +90,11 @@ class SignInVC: UIViewController {
         }
     }
     
-    func completeSignIn(id: String) {
+    func completeSignIn(id: String, userData: Dictionary<String, String>) {
+        DataService.ds.createFirebaseDBUser(uid: id, userData: userData)
         let keychainResult = KeychainWrapper.setString(id, forKey: KEY_UID)
         print("CHASE: Data saved to keychaise \(keychainResult)")
         performSegue(withIdentifier: "goToFeed", sender: nil)
     }
-
 }
 
