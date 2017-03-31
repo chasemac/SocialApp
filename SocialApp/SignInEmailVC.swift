8e5782f992f633ce3def656ed4170d4d3588a367
Chase McElroy <ichasem@gmail.com>
2017-03-31 14:12:15 -0700

    creating CreatePostVC for a full screen posting experience and creating more space on the FeedVC for the tableview

--- SocialApp/SignInEmailVC.swift
+++ SocialApp/SignInEmailVC.swift
@@ -10,37 +10,18 @@ import UIKit
 
 class SignInEmailVC: UIViewController, UITextFieldDelegate{
 
+
     @IBOutlet weak var emailField: FancyField!
+    @IBOutlet weak var pwdField: FancyField!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.emailField.delegate = self
+        self.pwdField.delegate = self
     }
     
     
-    
-    func checkEmailAddress(email: String) {
-        // Check if Username exist
-        
-        
-        DataService.ds.REF_USERS.child(EMAIL_DB_STRING).observeSingleEvent(of: .value, with: { (snapshot) in
-            if snapshot.hasChild(email) {
-                print("we're good")
-                
-                self.performSegue(withIdentifier: "accountExists", sender: nil)
-            } else {
-                print("username doesn't exist")
-                self.performSegue(withIdentifier: "accountDoesNotExists", sender: nil)
-            }
-        })
-    }
-
-    @IBAction func nextBtnTapped(_ sender: Any) {
-        if emailField.text != "" {
-            checkEmailAddress(email: emailField.text!)
-        }
-        
-    }
 
 }
